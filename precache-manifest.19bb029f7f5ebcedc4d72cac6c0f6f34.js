5069188caabad4472c67569ac22cf66794306abb
Code Sandbox <hello@codesandbox.io>
2022-02-27 23:03:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.19bb029f7f5ebcedc4d72cac6c0f6f34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c51e249708f96684ee483cd07cb8ad",
+    "url": "https://lilop243.github.io/csb-10n4dn/index.html"
+  },
+  {
+    "revision": "14ef8b861693df0b01b8",
+    "url": "https://lilop243.github.io/csb-10n4dn/static/css/main.c01be337.chunk.css"
+  },
+  {
+    "revision": "a4391989ec11d930a044",
+    "url": "https://lilop243.github.io/csb-10n4dn/static/js/2.8c84c990.chunk.js"
+  },
+  {
+    "revision": "913d4e2c9aac1c58eda0b0e0ede65b59",
+    "url": "https://lilop243.github.io/csb-10n4dn/static/js/2.8c84c990.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ef8b861693df0b01b8",
+    "url": "https://lilop243.github.io/csb-10n4dn/static/js/main.3ea2d46e.chunk.js"
+  },
+  {
+    "revision": "a03d1e4e873096e51083",
+    "url": "https://lilop243.github.io/csb-10n4dn/static/js/runtime-main.6adf3743.js"
+  }
+]);
